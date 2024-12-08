d0a614f65c1a573616148fa3383277b3970f6bbf
NongTham <naraberdee1@gmail.com>
2024-12-08 14:17:22 +0700

    new file:   Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+local placeId = game.PlaceId
+
+if (placeId == "3101667897"){
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NongTham/TomTamHub/refs/heads/main/Legends_Of_Speed.lua'))()
+} else {
+    print("Script not support")
+}
