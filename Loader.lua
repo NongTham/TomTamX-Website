1133b23d5670f954c4851f0967157fff5360937e
NongTham <naraberdee1@gmail.com>
2024-12-08 21:18:01 +0700

    modified:   Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,7 @@ local placeId = game.PlaceId
 if (placeId == 3101667897) then
     print("TomTamHub Loading : Legends Of Speed")
     loadstring(game:HttpGet('https://raw.githubusercontent.com/NongTham/TomTamHub/refs/heads/main/Legends_Of_Speed.lua'))()
-else if (placeId == 103289721276360) then
+elseif (placeId == 103289721276360) then
     print("TomTamHub Loading : Life in the Thai Military Fence [Roleplay]")
     loadstring(game:HttpGet('https://raw.githubusercontent.com/NongTham/TomTamHub/refs/heads/main/Life_in_the_Thai_Military_Fence_%5BRoleplay%5D.lua'))()
 else
