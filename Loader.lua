0ffe4d01ae557ff7fe220f8881330faaac5845c5
NongTham <naraberdee1@gmail.com>
2024-12-08 14:27:39 +0700

    modified:   Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,8 @@
 local placeId = game.PlaceId
 
-if (placeId == "3101667897") then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/NongTham/TomTamHub/refs/heads/main/Legends_Of_Speed.lua'))()
+if (placeId == 3101667897) then
     print("TomTamHub Loading : Legends Of Speed")
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NongTham/TomTamHub/refs/heads/main/Legends_Of_Speed.lua'))()
 else
     print("Script not support")
 end
